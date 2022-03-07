0c1e62169871652edadc0ac7ce56cd6ce9e13035
wei-ya-nan <soyongman0@gmail.com>
2022-03-07 15:56:19 +0800

    MyBatis的插件-分页插件

---
+++ MyBatis_demo3/src/test/java/com/wyn/test/PageHelperTest.java
@@ -0,0 +1,48 @@
+package com.wyn.test;
+
+import com.github.pagehelper.PageHelper;
+import com.wyn.bean.Emp;
+import com.wyn.mapper.EmpMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author wei-ya-nan
+ * @version 1.0
+ * @date 2022/3/7
+ */
+public class PageHelperTest {
+    /**
+     * limit index , pageSize
+     * index 当前页面的起始索引 从0 开始
+     * pageSize 每页显示的条数
+     *pageNum 当前页的页码
+     * index = （pageNum -1） * pageSize
+     *
+     */
+    @Test
+    public void testPage(){
+        try {
+            InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory build = new SqlSessionFactoryBuilder().build(resourceAsStream);
+            SqlSession sqlSession = build.openSession(true);
+            EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+
+            //开启分页功能
+            PageHelper.startPage(1,3);
+            List<Emp> emps = mapper.selectByExample(null);
+            emps.forEach(emp ->System.out.println(emp));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
